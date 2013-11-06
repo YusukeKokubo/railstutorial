f5d7cad8871d68fc164e7874ae426e3d337fc19e
YusukeKokubo <yusuke.kokubo@phantomtype.com>
2013-11-06 15:14:56 +0900

    improved with ApplicationHelper

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,6 +7,6 @@ describe "UserPages" do
     before { visit signup_path }
 
     it { should have_content('Sign up') }
-    it { should have_title("Ruby on Rails Tutorial Sample App | Sign up")}
+    it { should have_title(full_title("Sign up"))}
   end
 end
