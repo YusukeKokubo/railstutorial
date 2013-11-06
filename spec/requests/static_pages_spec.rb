26a1ddd8651a96e9cdac1a1fb1c0c7033c17a2a3
YusukeKokubo <yusuke.kokubo@phantomtype.com>
2013-11-06 15:05:15 +0900

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,41 +1,34 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  subject {page}
 
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content sample app" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the right title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    end
+    it { should have_content('Sample App') }
+    it { should have_title("Ruby on Rails Tutorial Sample App") }
+    it { should_not have_title("| Home") }
   end
 
   describe "Help page" do
-  	it "should have the content 'Help'" do
-  	  visit '/static_pages/help'
-  	  expect(page).to have_content('Help')
-  	end
+    before { visit help_path }
 
-    it "should have the right title" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title("Ruby on Rails Tutorial Sample App | Help") }
   end
 
   describe "About page" do
-  	it "should have the content 'About Us'" do
-		visit '/static_pages/about'
-	  	expect(page).to have_content('About Us')
-    end
+    before { visit about_path }
 
-    it "should have the right title" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
-    end
+    it { should have_content('About Us') }
+    it { should have_title("Ruby on Rails Tutorial Sample App | About Us") }
+  end
+
+  describe "Contract page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title("Ruby on Rails Tutorial Sample App | Contact") }
   end
 end
