761708a90abe776cbf53258e7fdd15e9d74d6ba2
YusukeKokubo <yusuke.kokubo@gmail.com>
2013-11-28 19:29:24 +0900

    Now

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ SampleApp::Application.routes.draw do
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
 
